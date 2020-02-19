33256f0c59979191ff84d44be68f8660662fb805
Mona Lisa <edgar.zavadski@gmail.com>
2020-02-19 13:11:00 +0200

    Updates

---
+++ precache-manifest.f07f5ca386e7780e7412aa75e2bb5d8a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "965fd118bffb0583e74362ffc0846cd9",
+    "url": "/myteam/index.html"
+  },
+  {
+    "revision": "a06a3a86183e93ffa4a0",
+    "url": "/myteam/static/css/main.772403cb.chunk.css"
+  },
+  {
+    "revision": "c586403b1f342a4a93ed",
+    "url": "/myteam/static/js/2.16dedccb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/myteam/static/js/2.16dedccb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a06a3a86183e93ffa4a0",
+    "url": "/myteam/static/js/main.4d800a16.chunk.js"
+  },
+  {
+    "revision": "6283a25efd138ab80506",
+    "url": "/myteam/static/js/runtime-main.e5bdbfe0.js"
+  },
+  {
+    "revision": "686d9685b805c407876cd1abd698fd73",
+    "url": "/myteam/static/media/Livvic-Regular.686d9685.ttf"
+  },
+  {
+    "revision": "d1d950e09ee94a2bbdd4627e65143cbc",
+    "url": "/myteam/static/media/avatar-aiysha.d1d950e0.jpg"
+  },
+  {
+    "revision": "60ff8909e4d598e25c144f0b47d988e3",
+    "url": "/myteam/static/media/avatar-arthur.60ff8909.jpg"
+  },
+  {
+    "revision": "f16826cacf2b1a460d2eab969d343d0a",
+    "url": "/myteam/static/media/avatar-kady.f16826ca.jpg"
+  },
+  {
+    "revision": "1ece178695707c353a82b30a19b11d2e",
+    "url": "/myteam/static/media/icon-chart.1ece1786.svg"
+  },
+  {
+    "revision": "6cb0f5760e3e8a0f4567755fb5250b32",
+    "url": "/myteam/static/media/icon-cog.6cb0f576.svg"
+  },
+  {
+    "revision": "05759150ed61077a65185636252594d7",
+    "url": "/myteam/static/media/icon-person.05759150.svg"
+  },
+  {
+    "revision": "efcf9a22216f31e99d42af206a8227a9",
+    "url": "/myteam/static/media/icon-quotes.efcf9a22.svg"
+  },
+  {
+    "revision": "22c2459ee9b2f81c568b15fdfa3e84bf",
+    "url": "/myteam/static/media/logo.22c2459e.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myteam/static/media/logo.5d5d9eef.svg"
+  }
+]);
